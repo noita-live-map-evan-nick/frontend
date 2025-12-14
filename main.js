11b029af84a4ee6dfca9f0cea973b5eea7e50c3a
Nick393 <82907413+Nick393@users.noreply.github.com>
2025-12-14 00:22:40 -0600

    change endpoint to https

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ for (const queryParamString of queryParamStrings) {
     }
 }
 
-let SERVER_ADDRESS = "http://noita-map-2-fqhnc6b2h6h4a2g9.canadacentral-01.azurewebsites.net";
+let SERVER_ADDRESS = "https://noita-map-2-fqhnc6b2h6h4a2g9.canadacentral-01.azurewebsites.net";
 
 
 // DOM elements
